9a0dc68b146d91bb1315016282ba978339fd45f6
Jyn Svetlana Martin-Schatz <jmartinschatz@gmail.com>
2018-10-21 17:38:11 -0400

    Update app-client.js

--- src/app-client.js
+++ src/app-client.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ClassSearch from "./components/classSearch";
 
-// This is where we're gonna actually "start" React on the client
-// Will probably only need to import Filter, Class (?)
-
-// the whole thing
-
-// TODO: keep track of the results count as a state
-
 ReactDOM.render(<ClassSearch />, document.getElementById("root"));
